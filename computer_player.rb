2afd91c8b44fdfe6ab7314de25a287bb59c07405
RJ Dabbar <rjdabbar@gmail.com>
2015-08-06 17:11:57 -0400

    Refactor ComputerPlayer# to be initialized with a board.

--- computer_player.rb
+++ computer_player.rb
@@ -1,9 +1,31 @@
 class ComputerPlayer
 
-  attr_reader :color
+  attr_reader :color, :board
 
-  def initialize(color)
-    @color = color
+  def initialize(color, board)
+    @color, @board = color, board
   end
+
+
+  def play_turn
+
+  end
+
+  def capture_move
+
+  end
+
+  def king_move
+
+  end
+
+  def safe_move
+
+  end
+
+  def random_move
+
+  end
+
 
 end
