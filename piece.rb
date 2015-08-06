691c8cde573ed0920e9f2ecafb4b9fcbbc39ad4e
RJ Dabbar <rjdabbar@gmail.com>
2015-08-06 11:04:39 -0400

    Refactor Piece#new_pos to Piece#new_slide and add Piece#jumps and Piece#new_jump.

--- piece.rb
+++ piece.rb
@@ -19,18 +19,34 @@ class Piece
     color == :red ? RED_MOVES : BLACK_MOVES
   end
 
+  def jumps
+    jumps = []
+    move_dirs.each do |direction|
+      potential_jump = new_jump(pos, direction)
+      blocked_space = new_slide(pos, direction)
+      if board.enemy?(blocked_space) && board.empty?(potential_jump)
+        jumps << potential_jump
+      end
+    end
+    jumps.select { |move| valid_move?(move) }
+  end
+
   def slides
     slides = []
       move_dirs.each do |direction|
-        potential_move = new_pos(pos, direction)
-        slides << potential_move if board.empty?(potential_move)
+        potential_slide = new_slide(pos, direction)
+        slides << potential_slide if board.empty?(potential_slide)
       end
 
     slides.select { |move| valid_move?(move) }
   end
 
-  def new_pos(start_pos, end_pos)
-    [start_pos[0] + end_pos[0], start_pos[1] + end_pos[1]]
+  def new_slide(start_pos, dir)
+    [start_pos[0] + dir[0], start_pos[1] + dir[1]]
+  end
+
+  def new_jump(start_pos, dir)
+    [start_pos[0] + dir[0]*2, start_pos[1] + dir[1]*2]
   end
 
   def valid_move?(move)
