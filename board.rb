719f3f5d95d3d67bd8de2d26e44636a1c3ed71d3
RJ Dabbar <rjdabbar@gmail.com>
2015-08-06 10:00:15 -0400

    Add Board#[] and Board#[]= methods.

--- board.rb
+++ board.rb
@@ -1,6 +1,16 @@
 class Board
-   attr_reader :rows
+   attr_reader :grid
   def initialize(filled=true)
 
+  end
+
+  def [](pos)
+    row, col = pos
+    grid[row][col]
+  end
+
+  def []=(pos, piece)
+    row, col = pos
+    grid[row][col] = piece
   end
 end
