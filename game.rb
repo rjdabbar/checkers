97805254fef0d64f36071588f5e554432929d484
RJ Dabbar <rjdabbar@gmail.com>
2015-08-06 15:01:19 -0400

    Clean up Game# and HumanPlayer#.

--- game.rb
+++ game.rb
@@ -1,15 +1,19 @@
+require_relative 'board.rb'
 class Game
+
+  attr_reader :players, :board
 
   def initialize
     @board = Board.new
     @players = {red: HumanPlayer.new(:red), black: HumanPlayer.new(:black)}
-
   end
 
 end
 
 
 class HumanPlayer
+
+  attr_reader :color
 
   def initialize(color)
     @color = color
