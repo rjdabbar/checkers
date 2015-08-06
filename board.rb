27242f4c77697ed1edef0e085120253dc893e71c
RJ Dabbar <rjdabbar@gmail.com>
2015-08-06 10:14:26 -0400

    Add Board#place_pieces and Board#populate_board.

--- board.rb
+++ board.rb
@@ -17,4 +17,25 @@ class Board
   def add_piece(piece)
     self[piece.pos] = piece
   end
+
+  def populate_board(filled)
+    @grid = Array.new(8) { Array.new(8) }
+    return unless filled
+    place_pieces
+  end
+
+  def place_pieces(color)
+    start_rows = [[0,1,2], [5,6,7]]
+    color == :red ? home = start_rows.shift : home = start_rows.pop
+
+    home.each do |row|
+      8.times do |col|
+        if row.even? && col.even? || row.odd? && col.odd?
+          Piece.new(color, self, [row, col])
+        end
+      end
+    end
+  end
+
+
 end
