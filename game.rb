dbe7d6dfe0661a1d946d023fccac30e8a86ddfd6
RJ Dabbar <rjdabbar@gmail.com>
2015-08-06 15:00:11 -0400

    Add HumanPlayer# and HumanPlayer#initialize to init with color.

--- game.rb
+++ game.rb
@@ -2,8 +2,17 @@ class Game
 
   def initialize
     @board = Board.new
-    @players = {red: HumanPlayer.new, black: HumanPlayer.new}
+    @players = {red: HumanPlayer.new(:red), black: HumanPlayer.new(:black)}
 
   end
 
+end
+
+
+class HumanPlayer
+
+  def initialize(color)
+    @color = color
+
+  end
 end
