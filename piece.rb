fc23a8389d6df7d83f8d799814507d750d31fcaa
RJ Dabbar <rjdabbar@gmail.com>
2015-08-06 10:01:39 -0400

    Refactor Piece#initialize to call Board#add_piece.

--- piece.rb
+++ piece.rb
@@ -2,9 +2,10 @@ class Piece
 
   attr_reader :color, :board
   attr_accessor :pos
-  
+
   def initialize(color, pos, board)
     @color, @pos, @board, = color, pos, board
+    board.add_piece(self)
   end
 
 end
